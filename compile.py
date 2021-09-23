103ae256d7e39e36f8cb60f814677c9e1d6f9ccd
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-09-23 07:13:06 -0400

    Finish compile script

--- compile.py
+++ compile.py
@@ -5,4 +5,13 @@ ubolist = ""
 lines = list.text.split("\n")
 for line in lines:
     if line.startswith("http"):
-      ubolist += urlparse(line)
+        queryparam = ""
+        if urlparse(line).query != "":
+            queryparam = "?" + urlparse(line).query
+        ubolist += "||" + urlparse(line).hostname +  urlparse(line).pathname + queryparam
+    else:
+        if line != "":
+            ubolist += "! " + line
+endfile = open("ubolist.txt","w")
+endfile.write(ubolist)
+endfile.close()
