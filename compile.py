4ce4445e211dcba09ac4d77f7d4554d43c46cb8f
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-10-12 13:30:18 -0400

    Update compile.py

--- compile.py
+++ compile.py
@@ -1,5 +1,6 @@
 import requests
 from urllib.parse import urlparse
+from hashlib import sha256
 list = requests.get("http://vxvault.net/URL_List.php")
 ubolist = """! Title: VXVault filter
 ! Description: VXVault's latest links compiled into a uBlock Origin compatible filter
@@ -7,6 +8,7 @@ ubolist = """! Title: VXVault filter
 ! Homepage: https://github.com/iam-py-test/vxvault_filter
 ! Data from http://vxvault.net/
 """
+sha256s = ""
 lines = list.text.split("\n")
 for line in lines:
     if line.startswith("http"):
@@ -14,9 +16,16 @@ for line in lines:
         if urlparse(line).query != "":
             queryparam = "?" + urlparse(line).query
         ubolist += "||" + urlparse(line).hostname +  urlparse(line).path + queryparam + "^$all\n"
+        try:
+            sha256s += "{}\n".format(sha256(requests.get(line)).hexdigest())
+        except:
+            pass
     else:
         if line != "" and "<pre>" not in line:
             ubolist += "! " + line + "\n"
 endfile = open("ubolist.txt","w")
 endfile.write(ubolist)
 endfile.close()
+with open("sha256s.txt","a") as f:
+    f.write(sha256s)
+    f.close()
