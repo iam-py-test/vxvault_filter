5c01ad9a9a997ebb7a7d924115970f546e009c41
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-09-11 15:43:49 -0400

    Weird

--- compile.py
+++ compile.py
@@ -34,7 +34,7 @@ for line in lines:
         ubolist += "||" + urlparse(line).hostname +  urlparse(line).path + queryparam + "^$all\n"
         if "||" + urlparse(line).hostname +  urlparse(line).path + queryparam not in fdata and fdata != "":
             try:
-               sha256 += sha256(requests.get(line).content).hexdigest() + "\n"
+               sha256s += sha256(requests.get(line).content).hexdigest() + "\n"
             except:
                 pass
     else:
