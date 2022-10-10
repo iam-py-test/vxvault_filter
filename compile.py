455ede1f1be697aed63194e3d269dcf5e15ac0d5
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-10-10 09:13:09 -0400

    Create the "full" version

--- compile.py
+++ compile.py
@@ -11,7 +11,7 @@ ubolist = """! Title: VXVault filter for uBlock Origin (unofficial)
 ! Homepage: https://github.com/iam-py-test/vxvault_filter
 ! Data from http://vxvault.net/
 """.format(date.today().strftime("%d/%m/%Y"))
-domains = """! Title: VXVault domains
+domains = """! Title: VXVault domains (unofficial)
 ! Description: A version of VxVault.net's latest malware urls containing only the domains of the offending urls
 ! Script last updated: 12/9/2022
 ! Expires: 1 day
@@ -23,6 +23,7 @@ try:
     all_urls_ever = open("all_urls_ever.txt")
 except:
     all_urls_ever = """! Title: VXVault filter for uBlock Origin (unofficial)
+! Expires: 1 day
 """
 
 sha256s = ""
@@ -41,6 +42,8 @@ for line in lines:
         if urlparse(line).query != "":
             queryparam = "?" + urlparse(line).query
         ubolist += "||" + urlparse(line).hostname +  urlparse(line).path + queryparam + "^$all\n"
+        if "||" + urlparse(line).hostname +  urlparse(line).path + queryparam + "^$all" not in all_urls_ever:
+            all_urls_ever += "||" + urlparse(line).hostname +  urlparse(line).path + queryparam + "^$all\n"
         if "||" + urlparse(line).hostname +  urlparse(line).path + queryparam not in fdata and fdata != "":
             try:
                print("LINE: ",line)
@@ -58,6 +61,10 @@ for line in lines:
 endfile = open("ubolist.txt","w")
 endfile.write(ubolist)
 endfile.close()
+
+all_urlsever = open("ubolist_full.txt",'w')
+all_urlsever.write(all_urls_ever)
+all_urlsever.close()
 
 safedomains = ["google.com","yahoo.com","duckduckgo.com","wikipedia.org","cdn.discordapp.com","discord.com","discordapp.com","raw.githubusercontent.com","lh3.google.com","drive.google.com","mediafire.com","download.com","googleusercontent.com","github.com","gitlab.com","avatars.githubusercontent.com","transfer.sh","download2264.mediafire.com","download2329.mediafire.com","download2340.mediafire.com","bit.ly","tiny.one",'rotf.lol',"onedrive.live.com","www90.zippyshare.com","www34.zippyshare.com","cdn.filesend.jp","pastebin.com","download2273.mediafire.com"]
 donedomains = []
